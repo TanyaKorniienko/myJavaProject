c9b7c5dc66e8f5e83b5826c9006042e7d6f177fe
Tanya <manualqa.tatiana@gmail.com>
2022-02-05 22:13:01 +0200

    Homework 2 Commit

---
+++ javaLessons/src/main/java/school/lessons/TestClass.java
@@ -0,0 +1,37 @@
+package school.lessons;
+
+public class TestClass {
+    public static void main(String[] args) {
+        //between10and20();
+        // findNum();
+    }
+    // int a = 5;
+    // int b = 7;
+    //  int sum = a + b;
+    //  if (sum > 10 && sum <= 20) {
+    //      System.out.println("True");
+    //  } else {
+    //      System.out.println("false");
+
+    //  }
+    //  public static boolean between10and20() {
+    //    int x = 8;
+    //    int y = 9;
+    //   if (x + y > 10 && x + y <= 20) {
+    //return true;
+    //        System.out.println("True");
+    //    } else {
+    //return false;
+    //       System.out.println("False");
+    // }
+    //}
+    // public static void findNum() {
+    //    int c;
+    ///    c = -22;
+    //    if (c < 0) {
+    //       System.out.println("Отрицательное число");
+    //    } else if (c >= 0) {
+    //    System.out.println("Положительное число");
+    //    }
+    //  }
+}
